a278f1542f2009a7f6dfa6b13f930e1e4108e65e
olmeiro <olme.orozco@gmail.com>
2023-05-25 09:23:21 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      'sm': { 'min': '320px' },
+      'md': { 'min': '768px' },
+      'lg': { 'min': '1024px' },
+      'xl': { 'min': '1280px' },
+    },
+  },
+  plugins: [],
+}
